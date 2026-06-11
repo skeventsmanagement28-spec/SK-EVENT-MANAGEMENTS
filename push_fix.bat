d8f034a31a61b3bfdf491ce07834841ecc7d59b7
bharani4822agmail.com <bharani4822a@gmail.com>
2026-06-11 23:04:16 +0530

    Remove push script

--- push_fix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d e:\Sk-management-v2.0
-git add -A
-git commit -m "Fix asset import casing for production build (PNG/JPG)"
-git remote set-url origin https://ghp_4crlhUVqlTAWe1EVKYpxgjPBlAeP0I24d7m@github.com/skeventsmanagement28-spec/SK-EVENT-MANAGEMENTS.git
-git push origin main
-echo DONE
